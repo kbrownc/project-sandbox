32e899747b37a044c842c57137ab00d526ba18c5
Kim Brown <kbrownc341@gmail.com>
2021-01-17 18:12:34 -0500

    After I added state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 
-let totalNum = 0;
-
 class App extends Component {
+  state = { totalNum: 0, getRandom: 0 }
+  
   getRandonNumber = (start, range) => {
-    let getRandom = Math.floor((Math.random() * range) +start);
-    console.log("Roll", getRandom);
-    return getRandom;
+    this.setState({getRandom: Math.floor((Math.random() * range) +start), });
+    console.log("Roll", this.state.getRandom);
+    return this.state.getRandom;
   }
 
   sumNumber = () => {
-    totalNum += (this.getRandonNumber(1,6));
-    if (totalNum > 30) {
+    this.setState({totalNum: this.state.totalNum + this.getRandonNumber(1,6), });
+    if (this.state.totalNum > 30) {
       let score = document.getElementById("score");
       score.style.visibility = "hidden";
     }
-    console.log("Running total", totalNum);
-    return totalNum;
+    console.log("Running total", this.state.totalNum);
+    return this.state.totalNum;
   }
 
 render() {
     return ( 
       <React.Fragment>
           <div><p>
-              <span>Score: </span><span>{this.totalNum}</span>
+              <span>Score: </span><span>{this.state.totalNum}</span>
           </p></div>
           <div><button id="score" onClick={() => this.sumNumber()}>Add</button></div>
           <div><p>
-              <span>Roll: </span><span>{this.getRandon}</span>
+              <span>Roll: </span><span>{this.state.getRandom}</span>
           </p></div>
       </React.Fragment>
       );
   }
 }
-
-// {this.sumNumber()}
-// {this.getRandonNumber(1,6)}
 
 export default App;
---
+++ src/Luke.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from 'react'
+
+const gameWinningPosition = 30;
+
+export default class AppClass extends React.Component {
+  state = { playerPosition: 0 }
+
+  onRoll = () => {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    this.setState((oldState) => ({
+      playerPosition: Math.min(oldState.playerPosition + roll, gameWinningPosition)
+    }))
+  }
+
+  render = () => {
+    const isGameDone = this.state.playerPosition >= gameWinningPosition;
+
+    return (
+      <div>
+        <span>{this.state.playerPosition}</span>
+        { isGameDone ? null : <button onClick={this.onRoll}>Roll</button> }
+      </div>
+    );
+  }
+}
+
+// Function Component Version
+// --------------------------
+
+// export default () => {
+//   const [playerPosition, setPlayerPosition] = useState(0);
+
+//   const onRoll = useCallback(() => {
+//     const roll = Math.floor(Math.random() * 6) + 1;
+//     setPlayerPosition((playerPosition) => Math.min(playerPosition + roll, gameWinningPosition))
+//   });
+
+//   const isGameDone = playerPosition >= gameWinningPosition;
+
+//   return (
+//     <div>
+//       <span>{playerPosition}</span>
+//       { isGameDone ? null : <button onClick={onRoll} >Roll</button> }
+//     </div>
+//   )
+// }
